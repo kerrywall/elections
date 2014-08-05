5b2beddd6363df14dfd9c860dc6de7c2ab20f93f
kerrywall <kerry.wall@gmail.com>
2014-08-05 18:05:58 -0400

    initial commit

---
+++ js/init.js
@@ -0,0 +1,109 @@
+// variables
+
+$showing = $('#showing');
+$results = $('#results');
+$list = $('#list');
+$phaseTwo = $('.phaseTwo');
+$search = $('#search');
+
+// submit the form
+
+$('document').ready(function() {
+	
+	// hide the search box
+
+	$phaseTwo.hide();
+
+	// set the search to elected candidates as a default
+
+	$userScope = "elected";
+
+	// figure out which set of data the people want
+
+	$('.scope').on('click', function() {
+
+		$userScope = $(this).val();
+
+	});
+
+	// determine what the user wants based on how they use the dropdown menu
+
+	$('#dropdown').on('change', function(d) {
+		d.preventDefault();
+		$phaseTwo.show();
+	    $userWants = "";
+	    $('.category:selected').each(function() {
+	    	// the user wants this!
+	    	$userWants = $(this).text();
+	    });
+	    
+	    // reset the results div and the search text box in case the user wants to start over
+
+	    $results.html("");
+	    $list.html("");
+	    $('#searchBox').val("");
+	    $('#showing').html("");
+
+	});
+
+	$('#search').on('submit', function(e) {
+		e.preventDefault();
+
+		$searchTerm = $('#searchBox').val();
+
+		$results.html("");
+		$list.html("");
+		
+		for (i = 0; i < candidates.length; i++) {
+
+			var thisCandidate = candidates[i];
+
+			function populate() {
+				if (thisCandidate[$userWants] == $searchTerm || thisCandidate[$userWants] == $searchTerm.toUpperCase()) {
+
+					var year = thisCandidate["Election Date"].substring(0,4),
+						name = thisCandidate["First Name"] + ' ' + thisCandidate["Last Name"],
+						party = thisCandidate["Party"],
+						province = thisCandidate["Province"];
+
+					if (party === "Conservative" || party === "Liberal-Conservative" || party === "Progressive Conservative" || party === "Government" || party === "National Government" || party === "Independent Conservative") {
+						partyClass = "cpc";
+					} else if (party === "Liberal" || party === "Opposition" || party === "Independent Liberal") {
+						partyClass = "lib";
+					} else if (party === "Co-operative Commonwealth Federation" || party === "NDP") {
+						partyClass = "ndp"; 
+					} else if (party === "Reform" || party === "Canadian Alliance") {
+						partyClass = "reform";
+					} else if (party === "Bloc Québécois") {
+						partyClass = "bq"; 
+					} else if (party === "Green") {
+						partyClass = "green"; 
+					} else if (party === "Social Credit") {
+						partyClass = "soCred";
+					} else {
+						partyClass = "other";
+					}
+
+					$list.append('<li class="names"><span class="name">' + name + '</span><span class="party">' + party + '</span><span class="province">' + province + '</span><span class="year">' + year + '</span></li>');
+
+					$results.append('<div class="candidate ' + partyClass + '" title="' + party + '"></div>');
+
+				}
+			}
+
+			if ($userScope === "elected") {
+				if (thisCandidate["Elected"] == 1) {
+					populate();
+			}
+			
+			} else {
+				if (thisCandidate[$userWants] == $searchTerm || thisCandidate[$userWants] == $searchTerm.toUpperCase()) {
+					populate();
+				}
+			}
+		}
+
+		$showing.html($userWants + ': <strong>' + $searchTerm + '</strong> (returned <strong>' + $results.children().length + '</strong> results among ' + $userScope + ' candidates).');
+
+	});
+});
